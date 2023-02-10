88b5addccc7b55e9868779b8b6348858db08a1dc
Juan Bispo <juanbispo02@gmail.com>
2023-02-10 11:01:19 -0300

    Implementação de finizalização de Orders - Projeto concluido com todos os endpoints

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -18,6 +18,7 @@ import { RemoveItemController } from './controllers/order/RemoveItemController';
 import { SendOrderController } from './controllers/order/SendOrderController';
 import { ListOrdersController } from './controllers/order/ListOrdersController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
 
 const router = Router();
 
@@ -58,6 +59,8 @@ router.put('/order/send',isAuthenticated, new SendOrderController().handle);
 
 router.get('/orders', isAuthenticated, new ListOrdersController().handle);
 
-router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle);
+
+router.put('/order/finish',isAuthenticated, new FinishOrderController().handle);
 
 export {router};
